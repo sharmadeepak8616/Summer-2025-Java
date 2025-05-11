761fce30dadbae9d218506b98996029566c5c0a4
Deepak <deepak>
2025-05-11 14:58:09 -0400

    Java Operators

---
+++ src/test/java/Class3/JavaOperators.java
@@ -0,0 +1,16 @@
+package Class3;
+
+public class JavaOperators {
+    public static void main(String[] args) {
+        /**
+         * Java Operators:
+         * 1. Assignment (=)
+         * 2. Arithmetic (+, -, *, /, %, ++, --)
+         * 3. Comparison (>, <. >=, <=, ==, !=)
+         * 4. Logical (&&, ||, !)
+         * 5. Advanced Assignment (+=, -=, *=, /=, %=)
+         * 6. Ternary (?:)
+         */
+
+    }
+}
